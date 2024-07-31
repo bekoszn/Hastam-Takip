ea4912e2ed01387b0777a473eb87f366dc5edeb0
bekoszn <berkeozguder@gmail.com>
2024-07-31 23:36:56 +0300

    Initial Commit

---
+++ Hastam Takip/Hastam_TakipApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hastam_TakipApp.swift
+//  Hastam Takip
+//
+//  Created by Berke Özgüder on 31.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Hastam_TakipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
